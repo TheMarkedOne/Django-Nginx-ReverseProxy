95a1c7e692826970897d444d954dd0dcb69d5d52
TheMarkedOne <zura.badzagua@makingscience.com>
2023-05-05 12:46:40 +0000

    ReverseProxyWithDjango

---
+++ skripti.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "# Django-Nginx-ReverseProxy" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://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@github.com/TheMarkedOne/Django-Nginx-ReverseProxy.git
+git push -u origin main
